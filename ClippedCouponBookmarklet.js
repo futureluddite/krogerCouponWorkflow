cf70e32b1ad30fb9d3ad409c749b9a4f6d9d8222
futureluddite <76464116+futureluddite@users.noreply.github.com>
2021-02-14 13:15:19 -0500

    Create ClippedCouponBookmarklet.js

---
+++ ClippedCouponBookmarklet.js
@@ -0,0 +1,45 @@
+
+ javascript:(function(){
+  function replaceCommonStrings(category) {
+    var replaceList = [
+      ["Simple Truth Organic","STO"],
+      ["Simple Truth","ST"],
+      ["Private Selection","PS"],
+      ["™",""],
+      ["®",""],
+      ["&amp;","&"]
+    ];
+    var map;
+    for (map of replaceList){
+      category = category.replace(map[0],map[1]);
+    };
+    return category;
+  }
+
+  function sortFunction(a, b) {
+    return (a[1] < b[1]) ? -1 : 1;
+  }
+
+  function main() {
+    var couponContainers = document.getElementsByClassName("CouponCardNew");
+    var cc, category, savings, description;
+    var list = [];
+    for (cc of couponContainers){
+      if (cc !== undefined) {
+        description = replaceCommonStrings(cc.getElementsByClassName("CouponCard-Description")[0].innerHTML.replace(/.*(on|off)[ 0-9]+/g,""));
+        savings     = cc.getElementsByClassName("CouponCard-Infos")[0].getElementsByTagName("h2")[0].getAttribute("aria-label").replace("Save ","");
+        category    = cc.getAttribute("data-category").slice(0, -1);
+        list.push([description + " (" + savings + ")\n",category]);
+      }
+    }
+    list = list.sort(sortFunction);
+    var row;
+    var ret = "";
+    for (row of list){
+      ret = ret + row[0];
+    }
+    alert(ret)
+  }
+
+  main();
+})();
