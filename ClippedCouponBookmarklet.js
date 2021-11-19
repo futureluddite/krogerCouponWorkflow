a616b4caf9fd16813e8aa8ccadb96bfcd2a5f362
futureluddite <76464116+futureluddite@users.noreply.github.com>
2021-11-19 16:34:27 -0500

    Remove unremovable Coupons

--- ClippedCouponBookmarklet.js
+++ ClippedCouponBookmarklet.js
@@ -21,6 +21,13 @@
   }
 
   function main() {
+    //Delete Coupons that have been removed
+    var removeNotices = document.getElementsByClassName("CannotRemoveCouponNew");
+    for (var i = removeNotices.length-1; i >= 0; i--) {
+      removeNotices[i].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
+    }
+
+    //Original code
     var couponContainers = document.getElementsByClassName("CouponCardNew");
     var cc, category, savings, description;
     var list = [];
